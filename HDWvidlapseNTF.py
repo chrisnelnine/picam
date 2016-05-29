ae41b3ce2fd8bbe617c66971947e03a92f4ce231
chrisnelnine <chrisnelnine@users.noreply.github.com>
2016-05-29 17:06:03 +0800

    Update HDWvidlapseNTF.py

--- HDWvidlapseNTF.py
+++ HDWvidlapseNTF.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
-#python HDvidlapseNTF.py NumberOfClips TimePerVidInSec FileName
+#HDvidlapseNTF.py takes three arguments NumberOfClips(N) TimePerVidInSec(T) FileName(F)
+#Records N video clips with resolution 1024 X 768, each of T seconds length with filename HD"F"000.h264, HD"F"001.h264 etc 
+#clips are found in the folderName folder
 
 import sys
 import time
 import picamera
 
+folderName = "/media/pi/WHITE/HD"
 numberOfClips = int(sys.argv[1])
 clipDuration = int(sys.argv[2]) #in seconds
 fileName = str(sys.argv[3])
@@ -16,7 +19,7 @@ with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     camera.hflip = True
     camera.vflip = False
-    for filename in camera.record_sequence("/media/pi/WHITE/HD" + str(fileName) +
+    for filename in camera.record_sequence(folderName + str(fileName) +
             '%02d.h264' % i for i in range(numberOfClips)):
         print('Recording to %s' % filename)
         camera.wait_recording(clipDuration)
